8547ad5cc01e7a23924346bfbf040645f8b86bf1
phillipe henriques <phillipe_ch@hotmail.com>
2022-10-01 00:20:16 -0300

    função cirada, html mudado

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,14 @@
 function createTable() {
-    var tbl = document.createElement('table');
+    var tabela = document.getElementsById('tabela')[0];
+    var tbl = document.createElement('table'); 
+
+    // tbl.setAttribute('class','' ) < colocar a classe da div > 
+
+    for (var row = 0; row <= 10; row++) {
+        var tr = document.createElement('tr');
+        for (var column = 0; column <= 1; column++) {
+            }
+        }
 }
+
+createTable()
