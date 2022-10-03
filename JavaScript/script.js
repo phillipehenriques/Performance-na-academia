2e17b9d043e29bcfc1711619d1e573d3ad8725e5
phillipe henriques <phillipe_ch@hotmail.com>
2022-10-02 22:56:44 -0300

    aprimorando JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,29 +1,47 @@
 function createTable() {
-    var tabela = document.getElementsById('tabela')[0];
-    var tbl = document.createElement('table'); 
 
-    // tbl.setAttribute('class','' ) < colocar a classe da div > 
+    var cabecalhosDaTabela = ['Mês', 'IMC', 'Massa gorda', 'Massa magra', 'Ossos', 'Resíduos', 'Músculos', 'água (%)', 'Taxa Metab.', 'Idade Metab.', 'Gordura Visc.']
 
-    for (var row = 0; row <= 10; row++) {
-        var tr = document.createElement('tr');
-        for (var column = 0; column <= 1; column++) {
-            }
-        }
-}
+    var tableContainer = document.getElementById('tableContainer');
+    var table = document.createElement('table');
+    table.id = 'tabeladeexercicios';
+    var thead = document.createElement('thead');
 
-createTable()
+    for (var index = 0; index < cabecalhosDaTabela.length; index ++) {
+        var th = document.createElement('th');
+        th.innerHTML = cabecalhosDaTabela[index]
+        // thead.setAttribute('class', 'alignmentLeft');
 
-function addResult() {
-    var resultados = document.getElementById('result')[0]
-    var coluna = document.createElement('row')
-
-    for (var linha = 0; linha < 11; linha++) {
-        var criarLinha = document.createElement('tr')
+        thead.appendChild(th);
     }
+
+    table.appendChild(thead);
+    tableContainer.appendChild(table);
+
+    populateTable();
 }
 
-    addResult()
+createTable();
 
-function addPage() {
-    
+// Chamado toda vez que a página é carregada e quando uma nova linha é adicionda
+
+
+//TODO: lição de casa
+// Chamada cada vez que o botão "adicionar" é acionado
+function createRow() {
+    // Procura documento de texto existente
+    // Se não existir, cria ele
+    //Se existir, adiciona novos dados ao documento
+    // Chama a função populateTable()
+    populateTable()
 }
+
+{/* <div id="tableContainer">
+    <table>
+        <thead>
+            <th>
+                Batata
+            </th>
+        </thead>
+    </table>
+</div> */}
