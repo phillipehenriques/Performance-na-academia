15644840e33363169b91bd1d3bbc7848705c2808
phillipe henriques <phillipe_ch@hotmail.com>
2022-10-16 20:04:16 -0300

    ultima atualização

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -25,92 +25,55 @@ function createTable() {
 
 createTable(); // < cabecalhosDaTabela porque não da pra colocar aqui ?????????????????????????????
 
-async function loadingLog() {
-    
-    try {
-        const arquivoJson = await fetch ('/arquivo.json')
-
-        arquivoJson.then((response) => response.json())    
-        arquivoJson.then((response) => {
-        return response.json();
-    })
-
-    } catch {
-        console.log('Erro ao carregar Json')
-    }
-
-    // import data from '/arquivo.json';
-    // console.log(data);
-
-
-    //return arquivoJson;
-}
-
-loadingLog()
-
 // Chamado toda vez que a página é carregada e quando uma nova linha é adicionda
 function populateTable(cabecalhosDaTabela) {
-    var tabela = document.getElementById('tabeladeexercicios');
+    json = '[{ "Mês": "valor", "IMC": "valor", "Massa gorda": "valor", "Massa magra": "valor", "Ossos": "valor", "Resíduos": "valor", "Músculos": "valor", "Água (%)": "valor", "Taxa Metab.": "valor", "Idade Metab.": "valor", "Gordura Visc.": "valor" }] '
+    var myJson = JSON.parse(json);
 
-    // var documentoDeTexto = leArquivoDeTexto();
-
-    // Para cada item no arquivo .json, adicionar uma linha com seus respectivos valores
-    for(var linha = 0; linha < documentoDeTexto.length; linha++) { 
-        var row = document.createElement('tr');
-
-        // Criando uma célula para cada coluna no array
-        for (var coluna = 0; coluna < cabecalhosDaTabela.length; coluna++) {
-            var celula = document.createElement('td');
-            // Escrevendo o valor dentro da celula
-            celula.innerHTML = documentoDeTexto[linha][coluna];
-            row.appendChild(celula, cabecalhosDaTabela);
-        }
-    }
+    console.log(myJson);
 }
-
-populateTable(celula, cabecalhosDaTabela)
 
 //TODO: lição de casa
 // Chamada cada vez que o botão "adicionar" é acionado
-function salvarLinha(celula) {
+// function salvarLinha(celula, cabecalhosDaTabela) {
 
-        var rowLogs = celula.value;
+//     var rowLogs = celula.value;
 
-        // for (var registro = 0; log >= cabecalhosDaTabela.length; )
+//     // for (var registro = 0; log >= cabecalhosDaTabela.length; )
 
-        if (rowLogs != null) {
-            ('/arquivo.json') = celula.value
-        }
-        else {
-            console.log ('erro ao carregar Json')
-        }
+//     if (rowLogs != null) {
+//         ('/arquivo.json') = celula.value
+//     }
+//     else {
+//         console.log ('erro ao carregar Json')
+//     }
 
-        Array.forEach(element => {
-            celula.innerHTML = element.value
-        });
+//     Array.forEach(element => {
+//         celula.innerHTML = element.value
+//     });
 
 
-        
-        // TENTANDO SALVAR ARQUIVO <<<<<<<<
+    
+//     // TENTANDO SALVAR ARQUIVO <<<<<<<<
 
-        var arquivo = new Blob([userInput], { type: "text/plain;charset=utf-8" });
-        saveAs(blob, "dynamic.txt");
-    }
+//     var arquivo = new Blob([userInput], { type: "text/plain;charset=utf-8" });
+//     saveAs(blob, "dynamic.txt");
+// }
 
-    // Procura documento de texto existente
-    // Se não existir, cria ele
-    //Se existir, adiciona novos dados ao documento
-    // Chama a função populateTable()
+//     // Procura documento de texto existente
+//     // Se não existir, cria ele
+//     //Se existir, adiciona novos dados ao documento
+//     // Chama a função populateTable()
 
-    populateTable()
+//     populateTable()
 
-{/* <div id="tableContainer">
-    <table>
-        <thead>
-            <th>
-                Batata
-            </th>
-        </thead>
-    </table>
-</div> */}
+// {/* <div id="tableContainer">
+//     <table>
+//         <thead>
+//             <th>
+//                 Batata
+//             </th>
+//         </thead>
+//     </table>
+// </div> */}
 
